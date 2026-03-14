d7595cc0d628a7a481eac922ad47bae587e28a8d
Abey Assefa <abey_haile@yahoo.com>
2026-03-14 07:32:46 +0000

    stage models created

---
+++ dbt_project/models/staging/backend/stg_backend__transactions_collection.sql
@@ -0,0 +1,17 @@
+{{ config(
+    materialized='view',
+    tags=['hourly']
+) }}
+
+SELECT
+    id AS collection_id,
+    account_id,
+    amount AS collection_amount,
+    created_at AS collection_created_at,
+    currency,
+    state AS collection_state,
+    to_wallet_id,
+    transaction_id,
+    updated_at AS collection_updated_at
+FROM
+    {{ source('backend', 'transactions_collection') }}
