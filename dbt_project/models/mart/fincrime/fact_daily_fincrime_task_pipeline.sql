41a37aba7238406e1da9bf8a277ecb4763545f73
Abey Assefa <abey_haile@yahoo.com>
2026-03-19 17:25:10 +0000

    update task pipline updated

---
+++ dbt_project/models/mart/fincrime/fact_daily_fincrime_task_pipeline.sql
@@ -0,0 +1,37 @@
+{{ config(
+    materialized='incremental',
+    unique_key='kpi_report_pk',
+    incremental_strategy='delete+insert'
+) }}
+
+WITH pipeline AS (
+    SELECT
+        task_created_date,
+        task_type,
+        primary_entity_type,
+        resolution,
+        workflow_execution_result,
+        COUNT(task_id) AS total_tasks_created,
+        COUNT(CASE WHEN task_state = 'RESOLVED' THEN 1 END) AS total_tasks_resolved,
+        AVG(minutes_to_resolution) AS avg_minutes_to_resolution,
+        COUNT(CASE WHEN workflow_execution_result = 'FAIL' AND resolution = 'APPROVED' THEN 1 END) AS total_false_positives,
+        COUNT(CASE WHEN workflow_execution_result = 'FAIL' AND resolution = 'REJECTED' THEN 1 END) AS total_true_positives
+    FROM 
+        {{ ref('int_fincrime__task_workflow_mapping') }}
+    {% if is_incremental() %}
+    WHERE task_updated_at >= DATEADD('day', -3, CURRENT_DATE)
+    {% endif %}
+    GROUP BY
+        task_created_date,
+        task_type,
+        primary_entity_type,
+        resolution,
+        workflow_execution_result
+)
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['task_created_date', 'task_type', 'primary_entity_type', 'resolution', 'workflow_execution_result']) }} AS kpi_report_pk,
+    *,
+    ROUND((total_tasks_resolved / NULLIF(total_tasks_created, 0)), 2) AS resolution_rate
+FROM 
+    pipeline
