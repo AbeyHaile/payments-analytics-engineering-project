2c27fcb187c7483210ad9fdc546c39ac27ab42d2
Abey Assefa <abey_haile@yahoo.com>
2026-03-26 11:33:41 +0000

    destination_country rename

--- dbt_project/models/mart/finance/fact_daily_transaction_corridor_performance.sql
+++ dbt_project/models/mart/finance/fact_daily_transaction_corridor_performance.sql
@@ -6,7 +6,7 @@ WITH daily_stats AS (
     SELECT
         DATE(created_at) AS metric_date,
         sent_currency AS source_country,
-        received_currency AS destination_country,
+        destination_country,
         corridor,
         COUNT(transaction_id) AS qualifying_transaction_attempts,
         COUNT(CASE WHEN transaction_state = 'COMPLETED' THEN transaction_id END) AS completed_qualifying_transactions,
